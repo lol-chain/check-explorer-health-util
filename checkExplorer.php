a05cac334d895567cdef00eadef32d99368ada00
root <root@vmi990062.contaboserver.net>
2022-09-06 11:17:29 +0200

    kekpoint: finalize logs, post-production test success; replicate test scenario by changing  to 100, for example...

--- checkExplorer.php
+++ checkExplorer.php
@@ -25,28 +25,28 @@ if(intVal($res_EXP) == intVal($res_RPC)){
         $txt = "EXPLORER IN SYNC: @ " . $date . " ";
         $myfile = file_put_contents('explorer_synced_logs.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
 } else {
-        echo 'EXPLORER NOT IN SYNC... RETRYING IN 10 SECONDS';
+        echo 'EXPLORER NOT IN SYNC... RETRYING IN 3 SECONDS';
 	sleep(3);
 	$res_EXP2 = checkExplorer();
 	$res_RPC2 = checkRPC();
 	if(intVal($res_EXP2) == intVal($res_RPC2)){
         	echo 'MATCH-SECOND-ATTEMPT';
 	} else {
-                echo 'EXPLORER NOT IN SYNC... RETRYING IN 10 SECONDS';
+                echo 'EXPLORER NOT IN SYNC... RETRYING IN 3 SECONDS';
 		sleep(3);
 		$res_EXP3 = checkExplorer();
         	$res_RPC3 = checkRPC();
         	if(intVal($res_EXP3) == intVal($res_RPC3)){
                 	echo 'MATCH-THIRD-ATTEMPT';
 		} else {
-                        echo 'EXPLORER NOT IN SYNC... RETRYING IN 10 SECONDS';
+                        echo 'EXPLORER NOT IN SYNC... RETRYING IN 4 SECONDS';
 			sleep(4);
 			$res_EXP4 = checkExplorer();
         		$res_RPC4 = checkRPC();
         		if(intVal($res_EXP4) == intVal($res_RPC4)){
                 		echo 'MATCH-FOURTH-ATTEMPT';
 			} else {
-				echo 'EXPLORER NOT IN SYNC... RETRYING IN 10 SECONDS';
+				echo 'EXPLORER NOT IN SYNC... RETRYING IN 5 SECONDS';
 				sleep(5);
 				$res_EXP5 = checkExplorer();
                         	$res_RPC5 = checkRPC();
