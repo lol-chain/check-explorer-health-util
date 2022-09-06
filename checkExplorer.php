987b7b34ab309cc0ce9bc2291d3b946195ffba7e
root <root@vmi990062.contaboserver.net>
2022-09-06 11:29:21 +0200

    add log cases for each err/success state

--- checkExplorer.php
+++ checkExplorer.php
@@ -17,7 +17,8 @@ $stop_command = "./exe_stop.sh";
 #sleep(20);
 # proceed to check EXP(explorer) && RPC(testnet.kekchain.com)
 $res_EXP = checkExplorer();
-$res_RPC = checkRPC();
+$res_RPC = 100000;
+	#checkRPC();
 # compare block numbers
 if(intVal($res_EXP) == intVal($res_RPC)){
 	echo 'MATCH';
@@ -25,33 +26,53 @@ if(intVal($res_EXP) == intVal($res_RPC)){
         $txt = "EXPLORER IN SYNC: @ " . $date . " ";
         $myfile = file_put_contents('explorer_synced_logs.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
 } else {
+	if(intVal($res_EXP) > intVal($res_RPC)){
+		echo 'EXPLORER IS ON MAIN CHAIN';
+		echo 'RPC SEEMS DELAYED';
+		$date = date('Y/m/d H:i:s');
+		$txt = "RPC NOT IN SYNC: @ " . $date . " ";
+		$myfile = file_put_contents('rpc_notsynced_logs.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
+		return $txt;
+	}
         echo 'EXPLORER NOT IN SYNC... RETRYING IN 3 SECONDS';
 	sleep(3);
 	$res_EXP2 = checkExplorer();
 	$res_RPC2 = checkRPC();
 	if(intVal($res_EXP2) == intVal($res_RPC2)){
-        	echo 'MATCH-SECOND-ATTEMPT';
+		echo 'MATCH-SECOND-ATTEMPT';
+		$date = date('Y/m/d H:i:s');
+		$txt = "EXPLORER IN SYNC: @ " . $date . " ";
+		$myfile = file_put_contents('explorer_synced_logs.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
 	} else {
                 echo 'EXPLORER NOT IN SYNC... RETRYING IN 3 SECONDS';
 		sleep(3);
 		$res_EXP3 = checkExplorer();
         	$res_RPC3 = checkRPC();
         	if(intVal($res_EXP3) == intVal($res_RPC3)){
-                	echo 'MATCH-THIRD-ATTEMPT';
+			echo 'MATCH-THIRD-ATTEMPT';
+			$date = date('Y/m/d H:i:s');
+			$txt = "EXPLORER IN SYNC: @ " . $date . " ";
+			$myfile = file_put_contents('explorer_synced_logs.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
 		} else {
                         echo 'EXPLORER NOT IN SYNC... RETRYING IN 4 SECONDS';
 			sleep(4);
 			$res_EXP4 = checkExplorer();
         		$res_RPC4 = checkRPC();
         		if(intVal($res_EXP4) == intVal($res_RPC4)){
-                		echo 'MATCH-FOURTH-ATTEMPT';
+				echo 'MATCH-FOURTH-ATTEMPT';
+				$date = date('Y/m/d H:i:s');
+				$txt = "EXPLORER IN SYNC: @ " . $date . " ";
+				$myfile = file_put_contents('explorer_synced_logs.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
 			} else {
 				echo 'EXPLORER NOT IN SYNC... RETRYING IN 5 SECONDS';
 				sleep(5);
 				$res_EXP5 = checkExplorer();
                         	$res_RPC5 = checkRPC();
                         	if(intVal($res_EXP5) == intVal($res_RPC5)){
-                                	echo 'MATCH-FIFTH-ATTEMPT';
+					echo 'MATCH-FIFTH-ATTEMPT';
+					$date = date('Y/m/d H:i:s');
+					$txt = "EXPLORER IN SYNC: @ " . $date . " ";
+					$myfile = file_put_contents('explorer_synced_logs.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
 				} else {
 					$date = date('Y/m/d H:i:s');
 					$txt = "EXPLORER NOT IN SYNC: REBOOTING @ " . $date . " ";
